0b1f350230122725f84efd6baef0538141892491
unknown <britol599@gmail.com>
2021-02-12 21:28:27 -0300

    Treino da rede

--- Main.py
+++ Main.py
@@ -6,15 +6,15 @@ out = pd.read_csv("Datasets/dataset_respostaTreino.csv")       #leitura do datas
 
 #definição dos parametros para treinamento da rede
 nDataFrame = df.shape[0]                                #retorna quantidade de elementos do dataset
-maxGeneration = 5000                                    #define o limite de gerações
+maxGeneration = 10000                                       #define o limite de gerações
 xGeneration = 0                                         #contador de gerações
 minError = 0.25                                         #define a taxa de erro permitida em cada teste no individuo
-learning_rate = 0.25                                    #taxa de aprendizado
-nNet = neuralNetwork(df, out, learning_rate)            #define construtor para rede neural
+learning_rate = 0.8                                       #taxa de aprendizado
+nNet = neuralNetwork(df, out, learning_rate)  #define construtor para rede neural
 
 #laço principal
 while xGeneration < maxGeneration:                      #while para contagem de gerações
-    nNet.enterData(nDataFrame, 0 , xGeneration)         #inicializa o processo de FeedForward para o individuo
+    nNet.enterData(nDataFrame, 0 , xGeneration)        #inicializa o processo de FeedForward para o individuo
     nNet.contador2 = 0;
     nNet.contador = 0;
 
